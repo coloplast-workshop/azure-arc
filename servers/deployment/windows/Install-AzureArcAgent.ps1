f9f0ea9de2dbccca330c6b2b7a2fd7620541b5e3
Lars Krogh Paulsen <lars.krogh.paulsen@atea.dk>
2021-12-08 14:05:57 +0100

    Minor update

--- servers/deployment/windows/Install-AzureArcAgent.ps1
+++ servers/deployment/windows/Install-AzureArcAgent.ps1
@@ -16,17 +16,13 @@ $env:tenantId = '<Azure tenant ID>'
 $env:resourceGroup = '<Azure resource group name>'
 $env:location = '<Azure Region>'
 
-function Get-MsiPackage()
+function Get-Package()
 {
   $ProgressPreference = 'SilentlyContinue'
   Invoke-WebRequest -Uri 'https://github.com/coloplast-workshop/azure-arc/raw/main/servers/packages/AzureConnectedMachineAgent.msi' -OutFile "$env:windir\Temp\AzureConnectedMachineAgent.msi"
 }
+Get-Package
 
-# Download Azure ARC agent MSI package
-Get-MsiPackage
-
-# Install Azure ARC agent
 & "$env:windir\system32\msiexec.exe" /i "$env:windir\Temp\AzureConnectedMachineAgent.msi" /l*v "$env:windir\Temp\AzureConnectedMachineAgent.txt" /qn | Out-String
 
-# Configure Azure ARC agent
 & "$env:ProgramFiles\AzureConnectedMachineAgent\azcmagent.exe" connect --service-principal-id $env:appId --service-principal-secret $env:password --resource-group $env:resourceGroup --tenant-id $env:tenantId --location $env:location --subscription-id $env:subscriptionId --tags 'Azure_ARC_servers' --correlation-id 'd009f5dd-dba8-4ac7-bac9-b54ef3a6671a'
