6cdbf3dbe806868bcc4942f185818898c66547b9
Lars Krogh Paulsen <lars.krogh.paulsen@atea.dk>
2021-12-09 15:07:11 +0100

    Minor update

---
+++ servers/scripts/prerequisites/007_New-AutomationAccount.ps1
@@ -0,0 +1,15 @@
+﻿<#
+    .SYNOPSIS   
+    Create a new Automation account
+
+    .NOTES
+    Name        : 007_New-AutomationAccount.ps1
+    Author      : Lars Krogh Paulsen
+    DateCreated : 2021-12-08
+    Version     : 0.0.1
+#>
+
+$resourceGroup = 'rg-azure-arc'
+$location = 'westeurope'
+
+New-AzAutomationAccount -ResourceGroupName $resourceGroup -Location $location -Name 'automation-account'
---
+++ servers/scripts/prerequisites/008_Enable-UpdateManagement.ps1
@@ -0,0 +1,5 @@
+﻿New-AzResourceGroupDeployment `
+    -Name DeployAALAUM `
+    -ResourceGroupName 'rg-azure-arc' `
+    -TemplateFile deployUMSolutiontemplate.json `
+    -_artifactsLocation "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/quickstarts/microsoft.automation/101-automation/azuredeploy.json"
