348e67d130a19f3d680523eab486186a8441410c
Lars Krogh Paulsen <lars.krogh.paulsen@atea.dk>
2021-12-13 17:48:54 +0100

    Minor update

--- servers/scripts/windows/Install-AzureArcAgent_v2.ps1
+++ servers/scripts/windows/Install-AzureArcAgent_v2.ps1
@@ -1,6 +1,6 @@
 ﻿<#
     .SYNOPSIS   
-    Use environment variables to install and configure the Azure ARC agent on Windows
+    Use environment variables and Chocolatey to install and configure the Azure ARC agent on Windows
 
     .NOTES
     Name        : Install-AzureArcAgent_v2.ps1
@@ -11,13 +11,22 @@
 
 function Get-Package()
 {
+  param(
+    [Parameter(Mandatory, Position = 0, HelpMessage = 'URL')]
+    [ValidateNotNullOrEmpty()]
+    [string]$url,
+    [Parameter(Mandatory, Position = 1, HelpMessage = 'Package')]
+    [ValidateNotNullOrEmpty()]
+    [string]$package
+  )
   $ProgressPreference = 'SilentlyContinue'
   Invoke-WebRequest -Uri ('{0}/{1}' -f $url, $package) -OutFile ('{0}/{1}' -f $localFolder, $package)
 }
 
-$localFolder = $env:SystemDrive + '\packages'
 $url = 'https://github.com/coloplast-workshop/azure-arc/raw/main/servers/packages'
 $packages = 'microsoft.azurearcagent.1.12.0.nupkg', 'microsoft.azureloganalyticsagent.11.0.9600.nupkg', 'microsoft.dependencyagent.9.10.11.nupkg'
+$localFolder = $env:SystemDrive + '\packages'
+$chocoExist = ((Test-Path -Path $env:ChocolateyInstall) -and (Test-Path -Path "$env:ChocolateyInstall\bin\Choco.exe"))
 
 if (-not (Test-Path -Path $localFolder))
 {
@@ -25,8 +34,17 @@ if (-not (Test-Path -Path $localFolder))
 }
 foreach ($package in $packages)
 {
-  Get-Package $url $package
+  Get-Package -url $url -package $package
 }
 
+if (-not ($chocoExist))
+{
+  $env:ChocolateyInstall = $env:programData + '\Chocolatey'
+  [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor 3072
+  Invoke-Expression -Command ((New-Object -TypeName 'System.Net.WebClient').DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+& "$env:ChocolateyInstall\bin\choco.exe" install 'microsoft.azureloganalyticsagent' -s "$env:SystemDrive\packages" -y
+& "$env:ChocolateyInstall\bin\choco.exe" install 'microsoft.dependencyagent' -s "$env:SystemDrive\packages" -y
 & "$env:ChocolateyInstall\bin\choco.exe" install 'microsoft.azurearcagent' -s "$env:SystemDrive\packages" -y
 & "$env:ProgramW6432\AzureConnectedMachineAgent\azcmagent.exe" connect --service-principal-id $env:appId --service-principal-secret $env:password --resource-group $env:resourceGroup --tenant-id $env:tenantId --location $env:location --subscription-id $env:subscriptionId  --verbose
