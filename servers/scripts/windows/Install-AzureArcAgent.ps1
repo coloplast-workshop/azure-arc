5a412344e1360f754e5f30eb3e8cb274af1cb37a
Lars Krogh Paulsen <lars.krogh.paulsen@atea.dk>
2021-12-13 21:16:46 +0100

    Minor update

--- servers/scripts/windows/Install-AzureArcAgent.ps1
+++ servers/scripts/windows/Install-AzureArcAgent.ps1
@@ -9,17 +9,15 @@
     Version     : 0.0.1
 #>
 
-$msiPackage = Join-Path -Path $env:windir -ChildPath 'Temp\AzureConnectedMachineAgent.msi'
-
 function Get-Package()
 {
   $ProgressPreference = 'SilentlyContinue'
   Invoke-WebRequest -Uri 'https://github.com/coloplast-workshop/azure-arc/raw/main/servers/packages/AzureConnectedMachineAgent.msi' -OutFile $msiPackage
 }
+
+$msiPackage = Join-Path -Path $env:windir -ChildPath 'Temp\AzureConnectedMachineAgent.msi'
+
 Get-Package
-
 & "$env:windir\system32\msiexec.exe" /i ('{0}' -f $msiPackage) /l*v "$env:windir\Temp\Install-AzureArcAgent.txt" /qn | Out-String
-
-& "$env:ProgramFiles\AzureConnectedMachineAgent\azcmagent.exe" connect --service-principal-id $env:appId --service-principal-secret $env:password --resource-group $env:resourceGroup --tenant-id $env:tenantId --location $env:location --subscription-id $env:subscriptionId  --verbose
-
-Remove-Item -Path $msiPackage -ErrorAction SilentlyContinue
+& "$env:ProgramFiles\AzureConnectedMachineAgent\azcmagent.exe" connect --tenant-id $env:tenantId --subscription-id $env:subscriptionId --resource-group $env:resourceGroup --location $env:location --service-principal-id $env:appId --service-principal-secret $env:password --verbose
+Remove-Item -Path $msiPackage -Force -ErrorAction SilentlyContinue
