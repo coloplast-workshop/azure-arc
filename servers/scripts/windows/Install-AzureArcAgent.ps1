8759e2487fc0b2deef50544a510de7a9bf08092c
Lars Krogh Paulsen <lars.krogh.paulsen@atea.dk>
2021-12-13 17:37:14 +0100

    Minor update

--- servers/scripts/windows/Install-AzureArcAgent.ps1
+++ servers/scripts/windows/Install-AzureArcAgent.ps1
@@ -1,6 +1,6 @@
 <#
     .SYNOPSIS   
-    Use environment variables to install and configure the Azure ARC agent on Windows
+    Use environment variables to install and configure the Azure ARC agent
 
     .NOTES
     Name        : Install-AzureArcAgent.ps1
@@ -9,12 +9,6 @@
     Version     : 0.0.1
 #>
 
-$env:subscriptionId = '<Azure subscription ID>'
-$env:appId = '<Azure service principal name>'
-$env:password = '<Azure service principal password>'
-$env:tenantId = '<Azure tenant ID>'
-$env:resourceGroup = '<Azure resource group name>'
-$env:location = '<Azure Region>'
 $msiPackage = Join-Path -Path $env:windir -ChildPath 'Temp\AzureConnectedMachineAgent.msi'
 
 function Get-Package()
@@ -26,6 +20,6 @@ Get-Package
 
 & "$env:windir\system32\msiexec.exe" /i ('{0}' -f $msiPackage) /l*v "$env:windir\Temp\Install-AzureArcAgent.txt" /qn | Out-String
 
-& "$env:ProgramFiles\AzureConnectedMachineAgent\azcmagent.exe" connect --service-principal-id $env:appId --service-principal-secret $env:password --resource-group $env:resourceGroup --tenant-id $env:tenantId --location $env:location --subscription-id $env:subscriptionId --correlation-id 'd009f5dd-dba8-4ac7-bac9-b54ef3a6671a'
+& "$env:ProgramFiles\AzureConnectedMachineAgent\azcmagent.exe" connect --service-principal-id $env:appId --service-principal-secret $env:password --resource-group $env:resourceGroup --tenant-id $env:tenantId --location $env:location --subscription-id $env:subscriptionId  --verbose
 
 Remove-Item -Path $msiPackage -ErrorAction SilentlyContinue
---
+++ servers/scripts/windows/Install-AzureArcAgent_v2.ps1
@@ -0,0 +1,32 @@
+﻿<#
+    .SYNOPSIS   
+    Use environment variables to install and configure the Azure ARC agent on Windows
+
+    .NOTES
+    Name        : Install-AzureArcAgent_v2.ps1
+    Author      : Lars Krogh Paulsen
+    DateCreated : 2021-12-08
+    Version     : 0.0.1
+#>
+
+function Get-Package()
+{
+  $ProgressPreference = 'SilentlyContinue'
+  Invoke-WebRequest -Uri ('{0}/{1}' -f $url, $package) -OutFile ('{0}/{1}' -f $localFolder, $package)
+}
+
+$localFolder = $env:SystemDrive + '\packages'
+$url = 'https://github.com/coloplast-workshop/azure-arc/raw/main/servers/packages'
+$packages = 'microsoft.azurearcagent.1.12.0.nupkg', 'microsoft.azureloganalyticsagent.11.0.9600.nupkg', 'microsoft.dependencyagent.9.10.11.nupkg'
+
+if (-not (Test-Path -Path $localFolder))
+{
+  $null = New-Item -Path $localFolder -ItemType 'Directory' -Force
+}
+foreach ($package in $packages)
+{
+  Get-Package $url $package
+}
+
+& "$env:ChocolateyInstall\bin\choco.exe" install 'microsoft.azurearcagent' -s "$env:SystemDrive\packages" -y
+& "$env:ProgramW6432\AzureConnectedMachineAgent\azcmagent.exe" connect --service-principal-id $env:appId --service-principal-secret $env:password --resource-group $env:resourceGroup --tenant-id $env:tenantId --location $env:location --subscription-id $env:subscriptionId  --verbose
